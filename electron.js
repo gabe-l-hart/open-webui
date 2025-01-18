862e45019cbd8a411ad3b06388fb5d46b9bd5c03
Gabe Goodhart <ghart@us.ibm.com>
2025-01-17 17:03:06 -0700

    feat: Add a _very_ simple electron main script
    Currently it doesn't launch the server process or have any control over the
    port
    
    Branch: Electron
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

---
+++ electron.js
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron'
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // Load your web UI into this window
+  win.loadURL('http://localhost:8080')
+
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
