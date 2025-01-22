5567e1207545d7c875edcea1167bea24125e8b69
Gabe Goodhart <gabe.l.hart@gmail.com>
2025-01-22 12:57:21 -0700

    build: Partial fix towards scripted windows build
    Some of the checks are not working and the conda env setup is still pretty
    piecemeal
    
    Signed-off-by: Gabe Goodhart <gabe.l.hart@gmail.com>

--- scripts/build-electron.sh
+++ scripts/build-electron.sh
@@ -7,6 +7,10 @@
 # Keep track of operating system and architecture the script is running on
 HOST_OS=$(uname -s)
 HOST_ARCH=$(uname -m)
+if [[ "$HOST_OS" == "MINGW"* ]]
+then
+    HOST_OS="Windows"
+fi
 
 # For now, the target OS and ARCH must match the host!
 TARGET_OS=$HOST_OS
@@ -17,8 +21,10 @@ help_str="Usage: $0 [options]
 Options:
     -h, --help         Show this help message
     -w, --working-dir  The working directory for the build
+    -f, --force        Force reinstall of open-webui in venv
     -c, --clean        Clean the working dir"
 working_dir="electron-build"
+force=0
 clean=0
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -29,6 +35,10 @@ while [ $# -gt 0 ]; do
         --working-dir|-w)
             working_dir="$2"
             shift 2
+            ;;
+        --force|-f)
+            force=1
+            shift
             ;;
         --clean|-c)
             clean=1
@@ -62,25 +72,47 @@ cd $working_dir
 # NOTE: Run in a subshell so that system python is not modified for
 #   electron-builder
 
+# Look for a working conda install across platforms
+conda_cmd=""
+if command -v conda &>/dev/null; then
+    conda_cmd="conda"
+elif command -v conda.bat &>/dev/null; then
+    conda_cmd="conda.bat"
+fi
+
+activate_cmd="$conda_cmd activate"
+
 # Get a version of conda to create the standalone env
-export PATH=$PWD/conda/bin:$PATH
-if ! command -v conda &> /dev/null
-then
+if [ "$conda_cmd" == "" ]; then
     echo "INSTALLING CONDA..."
     mkdir -p conda-tmp
     cd conda-tmp
+    conda_artifact="Miniforge3-${HOST_OS}-${HOST_ARCH}"
+    if [ "$HOST_OS" == "Windows" ]
+    then
+        conda_artifact="$conda_artifact.exe"
+    else
+        conda_artifact="$conda_artifact.sh"
+    fi
     curl -L -O https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/$conda_artifact
+    chmod +x $conda_artifact
+    ./$conda_artifact -b -p .conda
     cd ..
-    bash ${conda_artifact} -b -p .conda
 fi
 
 # Create the standalone python 3.11 environment with everything copied in
-conda create --yes python=3.11 -p $working_dir/venv --copy
-source activate $working_dir/venv
+if ! [ -d venv  ]; then
+    echo "CREATING venv..."
+    $conda_cmd create --yes python=3.11 -p venv --copy
+fi
+$activate_cmd $PWD/venv
 
 # Install open-webui from the source dir. This will also build the frontend and
 # bundle it with the python package.
-pip install $source_dir
+if ! command -v open-webui &>/dev/null || [ "$force" == "1" ]; then
+    echo "INSTALLING open-webui..."
+    pip install $source_dir
+fi
 
 # Figure out what the PYTHONPATH will need to be relative to the path the venv
 # will live in when installed.
